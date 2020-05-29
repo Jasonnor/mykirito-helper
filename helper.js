a73be146129afd183e98e20d3c3baf0593935949
Jasonnor <wujason810@gmail.com>
2020-05-29 23:42:52 +0800

    refactor: extract method clickEnabledBtnContainsText

--- helper.js
+++ helper.js
@@ -1,20 +1,24 @@
-function runJob() {
-    const actionBtn = Array.from(document.querySelectorAll('button:not([disabled])')).find(el => el.textContent === '汁妹')
-    if (actionBtn) {
-        console.log(`Click action`)
-        actionBtn.click()
-    }
-    const rewardBtn = Array.from(document.querySelectorAll('button:not([disabled])')).find(el => el.textContent === '領取獎勵')
-    if (rewardBtn) {
-        console.log(`Click reward`)
-        rewardBtn.click()
+function clickEnabledBtnContainsText(text) {
+    const button = Array
+        .from(document.querySelectorAll('button:not([disabled])'))
+        .find(el => el.textContent === text);
+    if (button) {
+        console.log(`Click ${text}`);
+        button.click();
     }
 }
+
+function runJob() {
+    clickEnabledBtnContainsText('汁妹');
+    clickEnabledBtnContainsText('領取獎勵');
+}
+
 (function loop() {
-    const duration = Math.random() * 10000 + 10000
-    console.log(`Wait ${(duration / 1000).toFixed(2)} sec before next action...`)
+    const duration = Math.random() * 10000 + 10000;
+    console.log(
+        `Wait ${(duration / 1000).toFixed(2)} sec before next action...`);
     setTimeout(function () {
-        runJob()
-        loop()
-    }, duration)
-}())
+        runJob();
+        loop();
+    }, duration);
+}());
