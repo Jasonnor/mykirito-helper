bf76f7bca36e420bdb959e3658be0909d31595d8
Jasonnor <wujason810@gmail.com>
2020-05-29 23:45:51 +0800

    chore: log action name

--- helper.js
+++ helper.js
@@ -13,7 +13,7 @@ let jobTextList = ['汁妹', '領取獎勵', '友好切磋'];
 function loopJob(jobText) {
     const duration = Math.random() * 10000 + 10000;
     console.log(
-        `Wait ${(duration / 1000).toFixed(2)} sec before next action...`);
+        `Wait ${(duration / 1000).toFixed(2)} sec before next ${jobText}...`);
     setTimeout(function () {
         clickEnabledBtnContainsText(jobText);
         loopJob(jobText);
