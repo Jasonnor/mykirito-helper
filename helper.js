ac93afce38f372a4b5622a41108cdc73886ca9bf
Jasonnor <wujason810@gmail.com>
2020-06-16 21:41:57 +0800

    feature: captcha detect and notify, close #1

--- helper.js
+++ helper.js
@@ -20,6 +20,21 @@ function loopJob(jobText) {
     }, duration);
 }
 
+function checkCaptcha() {
+    const duration = 20000;
+    setTimeout(function () {
+        if (document.querySelectorAll('iframe').length > 0) {
+            console.log('Captcha detected!');
+            new Notification(
+                '騙人的吧，又有 Captcha 了',
+                {'icon': 'https://i.imgur.com/qeoW9T5.png'});
+        }
+        checkCaptcha();
+    }, duration);
+}
+
 (function () {
+    Notification.requestPermission();
     jobTextList.forEach(jobText => loopJob(jobText));
+    checkCaptcha();
 })();
